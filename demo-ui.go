ad1342948cc5a16d418e930405be14d961e07d5e
padsterpat <padsterpat@gmail.com>
2014-08-31 17:37:45 +0200

    Demo UI hooked up to sine wave channel

--- demo-ui.go
+++ demo-ui.go
@@ -2,11 +2,25 @@
 package main
 
 import (
+	"math"
+	"time"
+
 	"github.com/padster/go-muse/ui"
 )
 
 func main() {
-	// TODO: Pipe in my own float64 channel.
-	s := ui.NewScreen(1000, 600)
-	s.Start()
+	// Generate a sum of three sin waves, 200Hz...
+	wave := make(chan float64)
+	go func() {
+		x := 0.0
+		for {
+			x += 0.01
+			wave <- (math.Sin(x) + math.Sin(2 * x) + math.Sin(4 * x)) / 3.0
+			time.Sleep(5 * time.Millisecond)
+		}
+	}()
+
+	// ...and draw them to screen. 
+	s := ui.NewScreen(1200, 600)
+	s.Render(wave, 3 /* sampleRate */)
 }
