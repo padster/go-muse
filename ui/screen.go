63f1ffa92b6a855880559e2da727446e0fe3d059
padster <padsterpat@gmail.com>
2015-07-17 17:54:13 -0700

    Updated screen display

--- ui/screen.go
+++ ui/screen.go
@@ -7,22 +7,25 @@ import (
 
 	"github.com/padster/go-muse/util"
 
-	"github.com/go-gl/gl"
-	glfw "github.com/go-gl/glfw3"
+	// TODO - migrate to core, not compat.
+	gl "github.com/go-gl/gl/v3.3-compatibility/gl"
+	glfw "github.com/go-gl/glfw/v3.1/glfw"
 )
 
 type Screen struct {
 	width  int
 	height int
+	pixelsPerSample float64 
 	buffer *util.Buffer
 }
 
 // NewScreen creates a new output screen of a given size.
-func NewScreen(width int, height int) *Screen {
+func NewScreen(width int, height int, squish int) *Screen {
 	s := Screen{
 		width,
 		height,
-		util.NewBuffer(width),
+		1.0 / float64(squish),
+		util.NewBuffer(width * squish),
 	}
 	return &s
 }
@@ -31,13 +34,22 @@ func NewScreen(width int, height int) *Screen {
 func (s *Screen) Render(values <-chan float64, sampleRate int) {
 	runtime.LockOSThread()
 
-	glfw.SetErrorCallback(func(err glfw.ErrorCode, desc string) {
-		log.Fatalf("%v: %s\n", err, desc)
-	})
-	if !glfw.Init() {
+	// TODO - error callback
+	// glfw.SetErrorCallback(func(err glfw.ErrorCode, desc string) {
+		// log.Fatalf("%v: %s\n", err, desc)
+	// })
+	if err := glfw.Init(); err != nil {
 		log.Fatalf("Can't init glfw!")
 	}
 	defer glfw.Terminate()
+
+	if err := gl.Init(); err != nil {
+		log.Fatalf("Can't init gl!")
+	}
+
+	// NOTE: Do not hint here, until fully migrated to -core.
+	// glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	// glfw.WindowHint(glfw.ContextVersionMinor, 3)
 
 	window, err := glfw.CreateWindow(s.width, s.height, "Muse", nil, nil)
 	if err != nil {
@@ -49,11 +61,11 @@ func (s *Screen) Render(values <-chan float64, sampleRate int) {
 	window.MakeContextCurrent()
 
 	// Set window up to be [0, -1.0] -> [width, 1.0], black.
-	gl.MatrixMode(gl.MODELVIEW_MATRIX)
+	gl.MatrixMode(gl.MODELVIEW)
 	gl.LoadIdentity()
 	gl.Translated(-1, 0, 0)
 	gl.Scaled(2/float64(s.width), 1.0, 1.0)
-	gl.ClearColor(0, 0, 0, 0)
+	gl.ClearColor(0.0, 0.0, 0.0, 0.0)
 
 	// Actually start writing data to the buffer.
 	s.buffer.GoPushChannel(values, sampleRate)
@@ -73,7 +85,7 @@ func (s *Screen) Render(values <-chan float64, sampleRate int) {
 func (s *Screen) drawSignal() {
 	gl.Begin(gl.LINE_STRIP)
 	s.buffer.Each(func(index int, value float64) {
-		gl.Vertex2d(float64(index), value)
+		gl.Vertex2d(float64(index) * s.pixelsPerSample, value)
 	})
 	gl.End()
 }
